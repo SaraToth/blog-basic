3cbd2d2e560d84767140c57e1257c00d1a4b7a83
SaraToth <saratoth101@gmail.com>
2025-08-18 08:59:42 +0900

    Feat: Create basic styles for BlogHome

---
+++ src/pages/BlogHome/BlogHome.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../../Components/Navbar";
+import "./BlogHome.css";
+
+const BlogHome = () => {
+    return(
+        <>
+            <Navbar/>
+            <div className="blog-home-content">
+                <p>This is the blog home</p>
+            </div>
+        </>
+    )
+};
+
+export default BlogHome;
