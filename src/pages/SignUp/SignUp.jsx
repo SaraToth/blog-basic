97236d9b4545039c0d33fab6a943b5fd87636ec7
SaraToth <saratoth101@gmail.com>
2025-08-18 15:48:05 +0900

    Feat: Finish signup handleSubmit logic

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,33 +1,61 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./SignUp.css";
 import Navbar from "../../components/Navbar";
+import { useState } from "react";
 
 const Signup = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+        try{
+            const res = await fetch("http://localhost:3000/user/signup", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ firstName, lastName, email, password, confirmPassword }),
+            });
+
+            const data = await res.json();
+            if (res.ok) {
+                navigate("/login");
+            } else {
+                alert(data.message || "Signup failed");
+            }
+        } catch(err) {
+            console.error(err);
+        };
+    }
     return(
         <>
             <Navbar/>
             <section>
                 <div className="form-container">
-                    <form action="http://localhost:3000/user/signup" method="post">
+                    <form onSubmit={handleSubmit}>
                         <div className="form-item">
                             <label htmlFor="firstName">First Name:</label>
-                            <input type="text" id="firstName" name="firstName" required/>
+                            <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} id="firstName" name="firstName" required/>
                         </div>
                         <div className="form-item">
                             <label htmlFor="lastName">Last Name:</label>
-                            <input type="text" name="lastName" id="lastName" required />
+                            <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} name="lastName" id="lastName" required />
                         </div>
                         <div className="form-item">
                             <label htmlFor="email">Email:</label>
-                            <input type="email" name="email" id="email" required/>
+                            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" required/>
                         </div>
                         <div className="form-item">
                             <label htmlFor="password">Password:</label>
-                            <input type="password" name="password" id="password" required/>
+                            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" required/>
                         </div>
                         <div className="form-item">
                             <label htmlFor="confirmPassword">Confirm Password:</label>
-                            <input type="password" name="confirmPassword" id="confirmPassword" required/>
+                            <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} name="confirmPassword" id="confirmPassword" required/>
                         </div>
 
                         <span>Already a member? <Link to="/login">Login</Link></span>
