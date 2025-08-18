7ca3eb89adfbd85e185e1fccbe214e86578b097f
SaraToth <saratoth101@gmail.com>
2025-08-18 15:02:42 +0900

    Feat: Update styles for login/signup pages

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,25 +1,28 @@
 import { Link } from "react-router-dom";
 import "./Login.css";
+import Navbar from "../../components/Navbar";
 
 const Login = () => {
     return(
-        <section>
-            <h1>Login to Words</h1>
-            <div className="form-container">
-                <form action="http://localhost:3000/user/login" method="post">
-                    <div className="form-item">
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" name="email" id="email" required />
-                    </div>
-                    <div className="form-item">
-                        <label htmlFor="password">Password:</label>
-                        <input type="password" name="password" id="password"/>
-                    </div>
-                    <span>Not a member? <Link to="/signup">Sign up</Link></span>
-                    <button type="submit">Login</button>
-                </form>
-            </div>
-        </section>
+        <>
+            <Navbar/>
+            <section>
+                <div className="form-container">
+                    <form action="http://localhost:3000/user/login" method="post">
+                        <div className="form-item">
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" name="email" id="email" required />
+                        </div>
+                        <div className="form-item">
+                            <label htmlFor="password">Password:</label>
+                            <input type="password" name="password" id="password"/>
+                        </div>
+                        <span>Not a member? <Link to="/signup">Sign up</Link></span>
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            </section>
+        </>
     )
 };
 
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,37 +1,41 @@
 import { Link } from "react-router-dom";
 import "./SignUp.css";
+import Navbar from "../../components/Navbar";
+
 const Signup = () => {
     return(
-        <section>
-            <h1>Sign up for Words</h1>
-            <div className="form-container">
-                <form action="http://localhost:3000/user/signup" method="post">
-                    <div className="form-item">
-                        <label htmlFor="firstName">First Name:</label>
-                        <input type="text" id="firstName" name="firstName" required/>
-                    </div>
-                    <div className="form-item">
-                        <label htmlFor="lastName">Last Name:</label>
-                        <input type="text" name="lastName" id="lastName" required />
-                    </div>
-                    <div className="form-item">
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" name="email" id="email" required/>
-                    </div>
-                    <div className="form-item">
-                        <label htmlFor="password">Password:</label>
-                        <input type="password" name="password" id="password" required/>
-                    </div>
-                    <div className="form-item">
-                        <label htmlFor="confirmPassword">Confirm Password:</label>
-                        <input type="password" name="confirmPassword" id="confirmPassword" required/>
-                    </div>
+        <>
+            <Navbar/>
+            <section>
+                <div className="form-container">
+                    <form action="http://localhost:3000/user/signup" method="post">
+                        <div className="form-item">
+                            <label htmlFor="firstName">First Name:</label>
+                            <input type="text" id="firstName" name="firstName" required/>
+                        </div>
+                        <div className="form-item">
+                            <label htmlFor="lastName">Last Name:</label>
+                            <input type="text" name="lastName" id="lastName" required />
+                        </div>
+                        <div className="form-item">
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" name="email" id="email" required/>
+                        </div>
+                        <div className="form-item">
+                            <label htmlFor="password">Password:</label>
+                            <input type="password" name="password" id="password" required/>
+                        </div>
+                        <div className="form-item">
+                            <label htmlFor="confirmPassword">Confirm Password:</label>
+                            <input type="password" name="confirmPassword" id="confirmPassword" required/>
+                        </div>
 
-                    <span>Already a member? <Link to="/login">Login</Link></span>
-                    <button type="submit">Sign up</button>
-                </form>
-            </div>
-        </section>
+                        <span>Already a member? <Link to="/login">Login</Link></span>
+                        <button type="submit">Sign up</button>
+                    </form>
+                </div>
+            </section>
+        </>
     )
 };
 
