b8fa65b204ec165099191ad9c674a43f04ad5c66
SaraToth <saratoth101@gmail.com>
2025-08-18 15:30:49 +0900

    Feat: Create login handleSubmit logic

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,21 +1,52 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 import Navbar from "../../components/Navbar";
+import { useState } from "react";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate(); // for redirecting
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try{
+            const res = await fetch("http://localhost:3000/user/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify({ email, password }),
+            });
+
+            const data = await res.json();
+
+            if (res.ok) {
+                // Store token locally
+                localStorage.setItem("token", data.token);
+
+                // Navigate to protected route
+                navigate("/blog");
+            } else {
+                alert(data.message || "Login failed");
+            }
+        } catch(err) {
+            console.error(err);
+        }
+    };
+
     return(
         <>
             <Navbar/>
             <section>
                 <div className="form-container">
-                    <form action="http://localhost:3000/user/login" method="post">
+                    <form onSubmit={handleSubmit}>
                         <div className="form-item">
                             <label htmlFor="email">Email:</label>
-                            <input type="email" name="email" id="email" required />
+                            <input type="email" value={email} name="email" id="email" required onChange={(e) => setEmail(e.target.value)}/>
                         </div>
                         <div className="form-item">
                             <label htmlFor="password">Password:</label>
-                            <input type="password" name="password" id="password"/>
+                            <input type="password" value={password} name="password" id="password" required onChange={(e) => setPassword(e.target.value)}/>
                         </div>
                         <span>Not a member? <Link to="/signup">Sign up</Link></span>
                         <button type="submit">Login</button>
