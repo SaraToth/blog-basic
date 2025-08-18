d8d3b37c69b9a2caf495d9a3d96575e5e922eef8
SaraToth <saratoth101@gmail.com>
2025-08-18 17:55:09 +0900

    Feat: Create blogpost page and css margins

---
+++ src/pages/BlogPost/BlogPost.jsx
@@ -0,0 +1,13 @@
+import "./BlogPost.css";
+
+const BlogPost = () => {
+    return(
+        <div className="blog-post">
+            <div className="blog-post-content">
+                <p>This is a blog post page</p>
+            </div>
+        </div>
+    )
+};
+
+export default BlogPost;
