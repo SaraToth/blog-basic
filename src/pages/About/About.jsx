0e8196c27be978fa95f94869697c77f3147b7dee
SaraToth <saratoth101@gmail.com>
2025-08-18 15:30:30 +0900

    Feat: Create example about me content

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,7 +5,9 @@ const About = () => {
         <div className="about-page">
             <div className="about-page-content">
             <h1>About</h1>
-            <p>My name is Sara.</p>
+            <p>Hi there! I'm the voice behind Words, a small corner of the internet where thoughts, stories, and ideas come to life. I love exploring the little details in everyday life and turning them into words that connect, inspire, and sometimes make you smile.
+            Whether it's reflections on life, tips for creativity, or just random musings, Words is my place to share what matters to me — and hopefully, it'll spark something in you too.
+            Thanks for stopping by, and feel free to explore, read, and join the conversation!</p>
             </div>
         </div>
     )
