c06cd270678d5c97e175a0bd7bca5dbde75d656a
SaraToth <saratoth101@gmail.com>
2025-08-18 16:38:26 +0900

    Feat: Create conditional navbar rendering

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,37 @@
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+    const token = localStorage.getItem("token");
+    const navigate = useNavigate();
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+        localStorage.removeItem("token");
+        navigate("/");
+    }
+
+    if (!token) {
+        return (
+            <nav>
+                <ul>
+                    <li><Link to="/">Words</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/blog">Blog</Link></li>
+                    <li><Link to="/signup">Signup</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </ul>
+            </nav>
+        )
+    }
+
     return(
         <nav>
             <ul>
                 <li><Link to="/">Words</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/blog">Blog</Link></li>
-                <li><Link to="/signup">Signup</Link></li>
-                <li><Link to="/login">Login</Link></li>
+                <li><Link onClick={handleLogout}>Logout</Link></li>
             </ul>
         </nav>
     )
